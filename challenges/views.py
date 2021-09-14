1f181638d13112ca362eed6004ecd741bd8c1123
Arslan Ameer <arslan.ameer@hotmail.com>
2021-09-14 17:16:56 +0500

    ♻️ Update: dynamic httpresponse, basic greystructure app

--- challenges/views.py
+++ challenges/views.py
@@ -13,6 +13,19 @@ monthly_challenges = {
 }
 
 
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href='{month_path}'>{capitalized_month}</a></li>"
+
+    response_data = f'<ul> {list_items} </ul>'
+    return HttpResponse(response_data)
+
+
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
 
