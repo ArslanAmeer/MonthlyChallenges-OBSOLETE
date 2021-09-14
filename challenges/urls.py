509c13212feb87164b43cf61a53465391c2de94b
Arslan Ameer <arslan.ameer@hotmail.com>
2021-09-14 14:47:18 +0500

    ✨ Add: dynamic url path

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('january', views.index)
+    path('<month>', views.monthly_challenges)
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,8 +1,21 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 
 # Create your views here.
 
 
-def index(response):
-    return HttpResponse('This works!')
+def monthly_challenges(request, month):
+    challenge_text = None
+
+    if month == "january":
+        challenge_text = 'January'
+
+    elif month == 'february':
+        challenge_text = 'February'
+
+    elif month == 'march':
+        challenge_text = 'March'
+
+    else:
+        return HttpResponseNotFound('Invalid month')
+    return HttpResponse(challenge_text)
