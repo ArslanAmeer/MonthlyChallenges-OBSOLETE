1615ed46cf9a961dd70e8165ba2af2cc7e4b1387
Arslan Ameer <arslan.ameer@hotmail.com>
2021-09-23 12:55:49 +0500

    ✨ Add: partial views

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
     path('<int:month>', views.monthly_challenge_by_number),
     path('<str:month>', views.monthly_challenge, name="month-challenge")
 ]
